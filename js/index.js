17434c9a77a9d8044d2bffd071ce4c2f6d22ab29
NCLAN\dtc <duongthuychuong@gmail.com>
2023-06-14 13:55:32 +0700

    add hero section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+window.onload = function () {
+  const navbar = document.getElementById("nav-container-id");
+  const allNavbarLinks = document.querySelectorAll(".nav-link");
+  window.onscroll = () => {
+    if (window.scrollY > 200) {
+      allNavbarLinks.forEach((link) => {
+        link.classList.add("nav-link-scroll");
+      });
+      navbar.classList.add("nav-active");
+    } else {
+      navbar.classList.remove("nav-active");
+      allNavbarLinks.forEach((link) => {
+        link.classList.remove("nav-link-scroll");
+      });
+    }
+  };
+};
+
 const openSidebar = () => {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.add("sidebar-show");
